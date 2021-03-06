75a0ee420e75c61591107cd19252179c8ca9fb1e
Ahmed Softev <ahmed_v@abv.bg>
2021-03-06 09:52:21 +0200

    EFC

---
+++ EntityRelations/P03 FootballBetting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using P03_FootballBetting.Data;
+
+namespace P03_FootballBetting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var context = new FootballBettingContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
