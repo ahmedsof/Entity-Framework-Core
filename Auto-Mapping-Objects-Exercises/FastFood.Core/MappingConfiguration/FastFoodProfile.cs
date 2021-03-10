268c0ef7c8d910ef392172d1bec107daacfeb8d0
Ahmed Softev <ahmed_v@abv.bg>
2021-03-10 22:09:40 +0200

    EF

---
+++ Auto-Mapping-Objects-Exercises/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,60 @@
+﻿using FastFood.Core.ViewModels.Categories;
+using FastFood.Core.ViewModels.Employees;
+using FastFood.Core.ViewModels.Items;
+using FastFood.Core.ViewModels.Orders;
+using Microsoft.VisualBasic;
+
+namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Models;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Employee
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(x => x.Position.Name));
+
+            // Category
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            //Item
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<CreateItemInputModel, Item>();
+
+            CreateMap<Item, ItemsAllViewModels>();
+
+            //Orders
+            this.CreateMap<CreateOrderInputModel, Order>()
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => DateAndTime.Now));
+
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => s.DateTime.ToString("d")));
+
+
+
+
+        }
+
+    }
+}
